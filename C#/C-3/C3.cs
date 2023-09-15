af120e071632bca768eca6f279df8138156eeda4
Kevin Barrera Hernández <kevbh@Kevins-MacBook-Pro.local>
2023-09-15 12:50:35 -0600

    CLS-113 Kevin Barrera Hernandez #comment C# challenge 3 completed

--- C#/C-3/C3.cs
+++ C#/C-3/C3.cs
@@ -4,15 +4,27 @@ class Program{
 
    static void Main(string[] args) {
 
-    
+
       Console.WriteLine("Enter the number:"); 
       int number = Console.Read();  
 
       /*
-      * write down your logic here
+      * I don't have something to run C# programs at my computer xD
+      * But here my solution without test it - adding comments to explain the logic
       */
- 
-           
+
+      int numberA = 0;
+      int numberB = 1;
+
+      for (int i = 0 ; i < number ; i++) {
+         Console.WriteLine(term1);
+
+         int nextNumber = numberA + numberB;
+         numberA = numberB;
+         numberB = nextNumber;
+      }
+
+
       Console.WriteLine("First terms of fibonacci series are:");
       Console.WriteLine(number);
    }
