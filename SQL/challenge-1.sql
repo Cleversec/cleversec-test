13c69856a8c1c6232728e26cb0af51ed38f9aeb6
Kevin Barrera Hernández <kevbh@Kevins-MacBook-Pro.local>
2023-09-15 10:23:32 -0600

    CLS-113 Kevin Barrera Hernandez #comment SQL challenge completed using MySQL syntax

--- SQL/challenge-1.sql
+++ SQL/challenge-1.sql
@@ -4,3 +4,5 @@
     2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
     3. Type your code immediately after comment. Don't leave any blank line.
 */
+SELECT CONCAT('(', UPPER(LEFT(Ocupation,1)), ')') FROM OCCUPATIONS ORDER BY Name;
+SELECT CONCAT('There are a lot of ', COUNT(Ocupation), LOWER(Ocupation), 's.') FROM OCCUPATIONS GROUP BY Ocupations ORDER BY COUNT(Ocupation), LOWER(Ocupation);
