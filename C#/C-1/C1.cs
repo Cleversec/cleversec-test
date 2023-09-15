a29df41a07435d59222a659329b83e139d473117
Kevin Barrera Hernández <kevbh@Kevins-MacBook-Pro.local>
2023-09-15 12:38:43 -0600

    CLS-113 Kevin Barrera Hernandez #comment C# challenge 1 completed

--- C#/C-1/C1.cs
+++ C#/C-1/C1.cs
@@ -1,25 +1,37 @@
 using System;
 
+
 class Program{
 
    static void Main(string[] args) {
-
-    
       Console.WriteLine("Enter the number:"); 
       int number = Console.Read();  
 
       Console.WriteLine("Enter the number to search:"); 
-      int searchnumber = Console.Read();
-               
+      int searchNumber = Console.Read();
+
       /*
-      * write down your logic here
+      * I don't have something to run C# programs at my computer xD
+      * But here my solution without test it - adding comments to explain the logic
       */
+
+      // turning number into string
+      string numberString = number.ToString();
+      // turning searched number into string
+      char searchChar = searchNumber.ToString();
+      // aux counter to increment each time that we found the searchChar appears on numberString
+      int counter = 0;
+
+      // Comparing each numberString digit with the searched number
+      foreach (char digit in numberString) {
+         if (digit == queryChar) {
+            counter++;
+         }
+      }
       
-               
       Console.WriteLine("Number of occurence of given number is:");
          
-      Console.WriteLine(number+searchnumber);
- 
+      Console.WriteLine(counter);
    }
 
 }
