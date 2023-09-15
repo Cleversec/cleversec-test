2726b12b4d7bb446b5eb5fbc881ce3ee12810060
Kevin Barrera Hernández <kevbh@Kevins-MacBook-Pro.local>
2023-09-15 11:21:25 -0600

    CLS-113 Kevin Barrera Hernandez #comment Angular challenge intial cleanning, initial routing and reactive forms module added

--- Angular/tecnhnical-test/src/app/app-routing.module.ts
+++ Angular/tecnhnical-test/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FillCardDetailsComponent } from './components/fill-card-details/fill-card-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: FillCardDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Angular/tecnhnical-test/src/app/app.module.ts
+++ Angular/tecnhnical-test/src/app/app.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FillCardDetailsComponent } from './components/fill-card-details/fill-card-details.component';
+import { CardPreviewComponent } from './components/card-preview/card-preview.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule
-  ],
+  declarations: [AppComponent, FillCardDetailsComponent, CardPreviewComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, NgbModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
