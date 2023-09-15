429082df4dcf9653408eb645436e2f69c4e1d9db
Kevin Barrera Hernández <kevbh@Kevins-MacBook-Pro.local>
2023-09-15 10:32:29 -0600

    CLS-113 Kevin Barrera Hernandez #comment JavaScript challenge 1 completed

--- Javascript/C-1/C-1.js
+++ Javascript/C-1/C-1.js
@@ -3,6 +3,36 @@
  * @param {number} size
  * @return {Array[]}
  */
-var chunk = function(arr, size) {
-    
+var chunk = function (arr, size) {
+  const newArray = [];
+  let currentIndex = 0;
+  while (currentIndex < arr.length) {
+    const chunk = arr.slice(currentIndex, currentIndex + size);
+    newArray.push(chunk);
+    currentIndex += size;
+  }
+  return newArray;
 };
+
+const tests = [
+  {
+    arr: [1, 2, 3, 4, 5],
+    size: 1,
+  },
+  {
+    arr: [1, 9, 6, 3, 2],
+    size: 3,
+  },
+  {
+    arr: [8, 5, 3, 2, 6],
+    size: 6,
+  },
+  {
+    arr: [],
+    size: 1,
+  },
+];
+
+tests.forEach((test, i) =>
+  console.log(`Test for example ${i + 1}`, chunk(test.arr, test.size))
+);
